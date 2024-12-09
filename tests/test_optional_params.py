9da68119efed2748e8a933c6d3f20c81621266a0
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-09 15:05:52 -0800

    tests: test all optional parameter options

---
+++ tests/test_optional_params.py
@@ -0,0 +1,20 @@
+from types import NoneType
+
+import yaflux as yf
+
+
+class OptionalParams(yf.Base):
+    """"""
+
+def test_params_null_input():
+    analysis = OptionalParams(parameters=None)
+    assert isinstance(analysis.parameters, NoneType)
+
+
+def test_params_no_input():
+    analysis = OptionalParams()
+    assert isinstance(analysis.parameters, NoneType)
+
+def test_params_some_input():
+    analysis = OptionalParams(parameters={"some": "input"})
+    assert isinstance(analysis.parameters, dict)
