f4640b4c555ac01b852e9eedba2ee280bf3dbb48
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-09 15:06:38 -0800

    style: ruff formatting

--- tests/test_optional_params.py
+++ tests/test_optional_params.py
@@ -6,6 +6,7 @@ import yaflux as yf
 class OptionalParams(yf.Base):
     """"""
 
+
 def test_params_null_input():
     analysis = OptionalParams(parameters=None)
     assert isinstance(analysis.parameters, NoneType)
@@ -14,6 +15,7 @@ def test_params_null_input():
 def test_params_no_input():
     analysis = OptionalParams()
     assert isinstance(analysis.parameters, NoneType)
+
 
 def test_params_some_input():
     analysis = OptionalParams(parameters={"some": "input"})
