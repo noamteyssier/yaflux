5d582d30ecab1d27d29d136e15913ec057699a1f
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-09 13:26:36 -0800

    feat: added a portable class which can be read when arbitrary classes are created

--- src/yaflux/__init__.py
+++ src/yaflux/__init__.py
@@ -1,4 +1,6 @@
 from ._base import Base
 from ._step import step
+from ._portable import Portable
+from ._loaders import load_analysis, load_portable, to_portable
 
-__all__ = ["Base", "step"]
+__all__ = ["Base", "step", "Portable", "load_analysis", "load_portable", "to_portable"]
