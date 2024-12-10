2d2907dfd42cca90d96a9df08fccd992e6faef2d
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-09 16:55:14 -0800

    tests: added testing on immutability

---
+++ tests/test_immutability.py
@@ -0,0 +1,48 @@
+import yaflux as yf
+
+
+class Analysis(yf.Base):
+
+    @yf.step(creates="res_a")
+    def step_a(self) -> int:
+        return 42
+
+    @yf.step(creates="res_b", requires="res_a")
+    def step_b(self) -> int:
+        return self.results.res_a * 2
+
+
+def test_immutable_append():
+    analysis = Analysis()
+    try:
+        analysis._results["res_a"] = 42 # type: ignore
+        assert False
+    except TypeError:
+        pass
+
+
+def test_immutable_setter():
+    analysis = Analysis()
+    try:
+        analysis.results["res_a"] = 42 # type: ignore
+        assert False
+    except TypeError:
+        pass
+
+def test_immutable_modify():
+    analysis = Analysis()
+    analysis.step_a()
+    try:
+        analysis.results["res_a"] = 43 # type: ignore
+        assert False
+    except TypeError:
+        pass
+
+def test_immutable_del():
+    analysis = Analysis()
+    analysis.step_a()
+    try:
+        del analysis.results["res_a"] # type: ignore
+        assert False
+    except TypeError:
+        pass
