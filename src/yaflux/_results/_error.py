3e0169bb0b94e6973ff55ed77b18b4b415c008c6
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-10 17:12:20 -0800

    feat: added flag errors

--- src/yaflux/_results/_error.py
+++ src/yaflux/_results/_error.py
@@ -2,3 +2,8 @@ class UnauthorizedMutationError(Exception):
     """Raised when attempting to modify results outside of a step decorator."""
 
     pass
+
+class FlagError(Exception):
+    """Raised when attempting to modify a flag that has already been set."""
+
+    pass
