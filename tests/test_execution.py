912a2d558b56239232933f1e498ba1103f3383f5
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-16 10:22:11 -0800

    tests: moved classes into test functions

--- tests/test_execution.py
+++ tests/test_execution.py
@@ -23,48 +23,12 @@ class ComplexAnalysis(yf.Base):
         return [self.results.res_c * 2 for _ in range(10)]
 
     @yf.step(creates="_mut_d", requires=["res_d", "rootless_res"])
-    def step_mut_d(self) -> list[int]:
+    def step_mut_d(self):
         self.results.res_d[0] = self.results.rootless_res
 
     @yf.step(creates="res_e", requires=["_mut_d", "res_d", "res_c", "res_b"])
     def step_e(self) -> int:
         return sum(self.results.res_d) * self.results.res_b + self.results.res_c
-
-
-class MissingStart(yf.Base):
-    @yf.step(creates="res_a", requires="res_b")
-    def step_a(self) -> int:
-        return self.results.res_b
-
-    @yf.step(creates="res_b", requires="res_a")
-    def step_b(self) -> int:
-        return self.results.res_a
-
-    @yf.step(creates="res_c", requires="res_a")
-    def step_c(self) -> int:
-        return self.results.res_a
-
-
-class CircularDownstream(yf.Base):
-    @yf.step(creates="res_a")
-    def step_a(self) -> int:
-        return 42
-
-    @yf.step(creates="res_b", requires="res_a")
-    def step_b(self) -> int:
-        _ = self.results.res_a
-        return 42
-
-    @yf.step(creates="res_c", requires=["res_d", "res_b"])
-    def step_c(self) -> int:
-        _ = self.results.res_d
-        _ = self.results.res_b
-        return 42
-
-    @yf.step(creates="res_d", requires="res_c")
-    def step_d(self) -> int:
-        _ = self.results.res_c
-        return 42
 
 
 def test_complex_analysis():
@@ -171,6 +135,19 @@ def test_empty_analysis():
 
 
 def test_non_dag_analysis_missing_start():
+    class MissingStart(yf.Base):
+        @yf.step(creates="res_a", requires="res_b")
+        def step_a(self) -> int:
+            return self.results.res_b
+
+        @yf.step(creates="res_b", requires="res_a")
+        def step_b(self) -> int:
+            return self.results.res_a
+
+        @yf.step(creates="res_c", requires="res_a")
+        def step_c(self) -> int:
+            return self.results.res_a
+
     analysis = MissingStart()
     # analysis.visualize_dependencies().render("missing_start", cleanup=True)
     try:
@@ -181,6 +158,27 @@ def test_non_dag_analysis_missing_start():
 
 
 def test_non_dag_analysis_circular_downstream():
+    class CircularDownstream(yf.Base):
+        @yf.step(creates="res_a")
+        def step_a(self) -> int:
+            return 42
+
+        @yf.step(creates="res_b", requires="res_a")
+        def step_b(self) -> int:
+            _ = self.results.res_a
+            return 42
+
+        @yf.step(creates="res_c", requires=["res_d", "res_b"])
+        def step_c(self) -> int:
+            _ = self.results.res_d
+            _ = self.results.res_b
+            return 42
+
+        @yf.step(creates="res_d", requires="res_c")
+        def step_d(self) -> int:
+            _ = self.results.res_c
+            return 42
+
     analysis = CircularDownstream()
     # analysis.visualize_dependencies().render("circular", cleanup=True)
     try:
