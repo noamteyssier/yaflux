e37fa0431f65f59fa385b2ce28ce5ab9173ab3a9
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-17 12:01:33 -0800

    feat: made compression an option for yax archives

--- src/yaflux/_yax/_tarfile.py
+++ src/yaflux/_yax/_tarfile.py
@@ -72,7 +72,9 @@ class TarfileSerializer:
         return "\n".join(_format_toml_section(manifest))
 
     @classmethod
-    def save(cls, filepath: str, analysis: Any, force: bool = False) -> None:
+    def save(
+        cls, filepath: str, analysis: Any, force: bool = False, compress: bool = False
+    ) -> None:
         """Save analysis to yaflux archive format.
 
         Parameters
@@ -84,10 +86,18 @@ class TarfileSerializer:
             Analysis object to save
         force : bool, optional
             Whether to overwrite existing file, by default False
+        compress : bool, optional
+            Whether to compress the archive (gzip), by default False
         """
         # Ensure correct extension
-        if not filepath.endswith(cls.EXTENSION):
-            filepath = filepath + cls.EXTENSION
+        if compress:
+            if not filepath.endswith(cls.EXTENSION + ".gz"):
+                filepath = filepath + cls.EXTENSION + ".gz"
+            if filepath.endswith(cls.EXTENSION):
+                filepath = filepath + ".gz"
+        else:
+            if not filepath.endswith(cls.EXTENSION):
+                filepath = filepath + cls.EXTENSION
 
         if not force and os.path.exists(filepath):
             raise FileExistsError(f"File already exists: '{filepath}'")
@@ -104,7 +114,8 @@ class TarfileSerializer:
         }
 
         # Create tarfile
-        with tarfile.open(filepath, "w:gz") as tar:
+        w_mode = "w:gz" if compress else "w"
+        with tarfile.open(filepath, w_mode) as tar:
             # Add metadata
             metadata_bytes = BytesIO(pickle.dumps(yax_metadata))
             metadata_info = tarfile.TarInfo(cls.METADATA_NAME)
@@ -162,7 +173,8 @@ class TarfileSerializer:
         select = cls._normalize_input(select)
         exclude = cls._normalize_input(exclude)
 
-        with tarfile.open(filepath, "r:gz") as tar:
+        r_mode = "r:gz" if filepath.endswith(".gz") else "r"
+        with tarfile.open(filepath, r_mode) as tar:
             # Load metadata
             metadata_file = tar.extractfile(cls.METADATA_NAME)
             if metadata_file is None:
