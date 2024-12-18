fe14104f9bf72ff6012af6bf4ec1a87d688a116a
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-12-18 09:48:32 -0800

    feat: added serialization support for pandas dataframes using pyarrow

--- src/yaflux/_yax/_serializer/__init__.py
+++ src/yaflux/_yax/_serializer/__init__.py
@@ -1,11 +1,17 @@
 import importlib.util
 
 from ._base import SerializerMetadata, SerializerRegistry
-from ._formats import AnnDataSerializer, NumpySerializer, PickleSerializer
+from ._formats import (
+    AnnDataSerializer,
+    NumpySerializer,
+    PandasSerializer,
+    PickleSerializer,
+)
 
 __all__ = [
     "AnnDataSerializer",
     "NumpySerializer",
+    "PandasSerializer",
     "PickleSerializer",
     "SerializerMetadata",
     "SerializerRegistry",
@@ -23,6 +29,13 @@ try:
     SerializerRegistry.register(NumpySerializer)
 except ImportError:
     pass  # Numpy is not installed
+
+try:
+    importlib.util.find_spec("pandas")
+    importlib.util.find_spec("pyarrow")
+    SerializerRegistry.register(PandasSerializer)
+except ImportError:
+    pass
 
 # Always register the pickle serializer last as a fallback
 SerializerRegistry.register(PickleSerializer)
